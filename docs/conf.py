3e199735d3bdbee4a339ec4d5d49495f129bc753
Richard Barker <dr.richard.barker@gmail.com>
2024-04-12 18:41:39 +0000

    help

--- docs/conf.py
+++ docs/conf.py
@@ -11,9 +11,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+ import os
+ import sys
+ sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
@@ -62,5 +62,5 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-/docs/conf.py at main 
+
 
